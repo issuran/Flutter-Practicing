7f2f4febbc7e1396a8fd99e8ef6048c07277d987
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2020-04-26 16:43:23 -0300

    Add image and fonts to pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -71,32 +71,18 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Clique aqui algumas vezes:',
+              style: TextStyle(
+                fontFamily: 'SourceSansPro',
+                fontSize: 20
+              ),
             ),
             Text(
               '$_counter',
@@ -104,6 +90,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   color: color,
                 fontSize: 20
               ),
+            ),
+            Container(
+              child: Image.asset('assets/background.jpg'),
             ),
           ],
         ),
